28ca5ad91f04cacb199025d4498954a5d5cf2373
Haojun Zou <apollo_zhj@msn.com>
2016-08-01 17:25:32 +0200

    hr_employee_date: new module added

---
+++ hr_employee_date/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   www.vertel.se
+#
+##############################################################################
+
+{
+    'name': 'Employee Contract Start Date',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'description': """
+Employee start date.
+=========================================================================================================
+    """,
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['hr_contract'],
+    'data': [
+        'hr_employee_date_view.xml',
+       ],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
