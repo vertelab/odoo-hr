b370234402d6241338a4bdb96bb615e43cea13f8
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-10-16 14:00:07 +0200

    hr_office: updated views, split out operations

--- hr_office/models/hr_department.py
+++ hr_office/models/hr_department.py
@@ -9,28 +9,37 @@ class HrDepartment(models.Model):
 
     office_code = fields.Char(string="Office code") #fyrställig
     organisation_number = fields.Char(string="Organisaiton Number") #sektionsnummer
-    location_ids = fields.Many2many(comodel_name='hr.location', string="Campuses")
+    operation_ids = fields.Many2many(comodel_name='hr.operation', string="Operations")
     partner_id = fields.Many2one(comodel_name="res.partner")
     
 
 
-class Hrlocation(models.Model):
-    _name = "hr.location"
+class HrOperation(models.Model):
+    _name = "hr.operation"
 
     name = fields.Char(string="Name")
     opening_hours = fields.Char(string = 'Opening hours')
     personal_service_opening = fields.Char(string="Opening hours for personal service")
-    workplace_number = fields.Char(string="Workplace number")
-    location_code = fields.Char(string="Location code")
     x500_id = fields.Char(string="x500 id")
     
-    department_ids = fields.Many2many(comodel_name='hr.department', string="Office")
-    
+    department_id = fields.Many2one(comodel_name='hr.department', string="Office")
+    accessibilites_ids = fields.One2many(comodel_name='hr.location.accessibility', inverse_name='location_id')
+
     partner_id = fields.Many2one('res.partner', string="Partner") 
     visitation_address_id = fields.Many2one('res.partner', string="Visitation address")
     mailing_address_id = fields.Many2one('res.partner', string="Mailing address")
 
-    accessibilites_ids = fields.One2many(comodel_name='hr.location.accessibility', inverse_name='location_id')
+    location_id = fields.Many2one(comodel_name='hr.location', string="Location")
+
+class HrLocation(models.Model):
+
+    name = fields.Char(string="Name")
+    location_code = fields.Char(string="Location code")
+    workplace_number = fields.Char(string="Workplace number")
+
+    operation_ids = fields.One2many(comodel_name='hr.operation', string="Operations", inverse_name='location_id')
+
+    user_ids = fields.One2many(comodel_name='res.users', string="Users", inverse_name='location_id')
 
     @api.model
     def get_workplace_number(self, location_code):
@@ -39,6 +48,7 @@ class Hrlocation(models.Model):
             return location.workplace_number
         else:
             return False
+
 
 class HrlocationAccessibility(models.Model):
     _name = "hr.location.accessibility"
