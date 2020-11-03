551b6f8dea8c858adf426422381bc1547a013dde
Miracle Ayodele <miracleaayodele@gmail.com>
2020-11-03 02:37:36 +0100

    updated data

---
+++ fenix_test_hr/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fenix Test HR",
+    "version": "12.0.1.0.1",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
+        "base",
+        "hr",
+    ],
+    "external_dependencies": [],
+    "data": [
+        'data/hr_employee.xml',
+
+    ],
+    "application": True,
+    "installable": True,
+}
