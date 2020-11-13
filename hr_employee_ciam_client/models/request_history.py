5bc71a52c56fcede656e971434e7105db9969de8
Nils <nils.nyman-waara@vertel.se>
2020-11-13 10:15:03 +0000

    hr_employee_ciam_client,hr_employee_ciam_server 12.0-AFC-1330:Adding modules ciam_client and ciam_server
    * CIAM_Client for AF project, API module to handle communication between FENIX and CIAM
    * CIAM_Server for AF project, API module for testing of CIAM_client
    * Updated .gitignore with ignores for Python

---
+++ hr_employee_ciam_client/models/request_history.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019 N-Development (<https://n-development.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import logging
+from odoo import api, http, models, tools, SUPERUSER_ID, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class RequestHistory(models.Model):
+    _name = 'request.history'
+    _rec_name = 'url'
+
+    config_id = fields.Many2one('ciam.client.config',
+                                string="Config ID")
+    url = fields.Char(string='Url')
+    method = fields.Char(string='Method')
+    payload = fields.Char(string='Payload')
+    request_headers = fields.Char(string='Request Headers')
+    response_headers = fields.Char(string='Response Headers')
+    params = fields.Char(string='Params')
+    response_code = fields.Char(string='Response Code')
+    message = fields.Char(string='Message')
