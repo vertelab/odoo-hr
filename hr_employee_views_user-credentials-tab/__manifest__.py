ea31930410728ef9168ed816be2723fb91c729b1
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-14 14:01:25 +0530

    [ADD] hr_employee_views_user-credentials-tab, hr_employee_views_fenix: AFC-1279 Added new modules hr_employee_views_fenix and hr_employee_views_user-credentials-tab to add and hide some fields inside employee views.

---
+++ hr_employee_views_user-credentials-tab/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'HR Employee views - User Credential tab',
+    'version': '12.0.0.0',
+    'category': 'Human Resources',
+    'summary': 'Adds User-Credentials Tab in employee view.',
+    'description': 'Adds User-Credentials Tab in employee view.',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se',
+    'depends': [
+        'hr'
+    ],
+    'data': [
+        'views/hr_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
