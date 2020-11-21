23eaeff601b252d0388a69aee393e04071f63adb
Fredrik Arvas <fredrik@arvas.se>
2020-11-21 21:43:59 +0100

    Update __manifest__.py

--- hr_office/__manifest__.py
+++ hr_office/__manifest__.py
@@ -1,10 +1,11 @@
 {
     'name': 'HR Department offices',
-    'version': '12.0.0.2',
+    'version': '12.0.0.3',
     'category': 'Human resources',
       "description": """
     Offices
-    12.0.0.2 views
+    12.0.0.2 views \n
+    12.0.0.3 added group_no_one to menues to hide them from internal users \n
     """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
@@ -16,4 +17,5 @@
       'security/ir.model.access.csv'
     ],
     'installable': True,
+    'application': False,
 }
