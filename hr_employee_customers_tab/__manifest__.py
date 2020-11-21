18d29098c4c36f972bb06d34579757d1fc9c0f8a
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-21 15:28:05 +0530

    [ADD] hr_employee_customers_tab: AFC-1279 Added module hr_employee_customers_tab.

---
+++ hr_employee_customers_tab/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "HR Jobseekers Tab",
+    "version": "12.0.0.0",
+    "author": "Vertel AB",
+    "description": """
+        This module adds a tab with "My customers" on the "HR-employee"-form.\n
+    """,
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "depends": [
+        'sale', 'hr'
+    ],
+    "category": "HR",
+    "data": [
+        'views/hr_views.xml',
+    ],
+    "application": False,
+    "installable": True,
+}
+
