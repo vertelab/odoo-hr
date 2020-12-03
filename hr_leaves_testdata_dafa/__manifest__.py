b9133ad153da95e617c39e49e7cb0acfe1452277
Miracle Ayodele <miracleaayodele@gmail.com>
2020-12-03 07:26:18 +0100

    renamed fenix_test_hr_leaves and fixed bug

--- hr_leaves_testdata_dafa/__manifest__.py
+++ hr_leaves_testdata_dafa/__manifest__.py
@@ -1,13 +1,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Fenix Test HR Leave Test",
-    "version": "12.0.1.0.1",
+    "name": "DAFA Test HR Leave Test",
+    "version": "12.0.1.0.2",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
     "depends": [
         "base",
+        "hr",
         "hr_holidays",
         "hr_test-data_fenix",
     ],
