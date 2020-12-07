e2ff967de8bc95c2e93fbb61be795e428a7370ae
Fredrik Arvas <fredrik@arvas.se>
2020-12-07 17:01:22 +0100

    corrected error in description

--- hr_employee_views_user-credentials-tab/__manifest__.py
+++ hr_employee_views_user-credentials-tab/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '12.0.0.1',
     'category': 'Human Resources',
     'summary': 'Replaced by auth_user_rights_wizard.',
-    'description': 'Adds User-Credentials Tab in employee view.' Module is r,
+    'description': 'Adds User-Credentials Tab in employee view.',
     'author': 'Vertel AB',
     'website': 'https://vertel.se',
     'depends': [
