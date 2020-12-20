0af66f02544801346dae94acec18558b2cd9bf71
Artur Chilangwa <artur.chilangwa@vertel.se>
2020-12-20 10:31:01 +0100

    [MOV] hr_employee_views_fenix: AFC-1578 Depricate module
    Module has been renamed to hr_employee_views_dafa

--- hr_employee_views_fenix/__manifest__.py
+++ hr_employee_views_fenix/__manifest__.py
@@ -1,9 +1,12 @@
 {
-    'name': 'HR Employee views',
+    'name': 'Depreciated - HR Employee views',
     'version': '12.0.0.0',
     'category': 'Human Resources',
     'summary': 'Hide some fields from Employee view and added smart button to show emplyee is present or not.',
-    'description': 'Hide some fields from Employee view and added smart button to show emplyee is present or not.',
+    'description': """
+        This module has been moved to hr_employee_views_dafa
+        Hide some fields from Employee view and added smart button to show emplyee is present or not.
+    """,
     'author': 'Vertel AB',
     'website': 'https://vertel.se',
     'depends': [
@@ -12,6 +15,6 @@
     'data': [
         'views/hr_views.xml',
     ],
-    'installable': True,
+    'installable': False,
     'auto_install': False,
 }
