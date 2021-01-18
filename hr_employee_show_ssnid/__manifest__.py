e099fa56b201a77d8da8a382dac446af6ff5792b
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-18 11:26:22 +0100

    hr_employee_show_ssnid: module that shows the hidden ssnid field

---
+++ hr_employee_show_ssnid/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Employee show SSN',
+    'version': '12.0.0.1.1',
+    'category': 'Generic Modules/Human Resources',
+    'author': "Vertel AB",
+    'website': 'Vertel.se',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'views/hr_view.xml',
+    ],
+    'installable': True,
+}
