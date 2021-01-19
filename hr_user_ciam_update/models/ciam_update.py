e71ea31e17ac4ec9ba27fc5ad2f11349ae3d6ce1
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-19 10:10:31 +0100

    hr_user_ciam_update: pep8ed the code

--- hr_user_ciam_update/models/ciam_update.py
+++ hr_user_ciam_update/models/ciam_update.py
@@ -9,21 +9,22 @@ _logger = logging.getLogger(__name__)
 class CIAMUpdate(models.TransientModel):
     _name = 'hr.employee.user.ciam'
 
-    employee_id = fields.Many2one('hr.employee', default=lambda self: self.env.context.get('active_id'))
+    employee_id = fields.Many2one(
+        'hr.employee', default=lambda self: self.env.context.get('active_id'))
 
     def action_update_ciam(self):
         ciam_id = self.env['ciam.client.config'].search([], limit=1)
         if ciam_id:
             data = {
-                'personNr': self.employee_id.ssnid if self.employee_id.ssnid else "197001011234",
+                'personNr': self.employee_id.ssnid if self.employee_id.ssnid else "197001011234",  # noqa:E501
                 'firstName': self.employee_id.firstname,
                 'lastName': self.employee_id.lastname,
                 'eMail': self.employee_id.user_id.email,
                 'username': self.employee_id.user_id.login,
-                #commented password in case we need it back for testing later
-                #password is required for test environments
-                #'password': self.employee_id.user_id.password if self.employee_id.user_id.password else "Acctest09",
-                #'customerNr': self.employee_id., #not implemented yet
+                # commented password in case we need it back for testing later
+                # password is required for test environments
+                # 'password': self.employee_id.user_id.password if self.employee_id.user_id.password else "Acctest09",  # noqa:E501
+                # 'customerNr': self.employee_id., #not implemented yet
                 'status': '1'
                 }
             response = ciam_id.user_add(data)
@@ -37,7 +38,7 @@ class CIAMUpdate(models.TransientModel):
                 status = res_dict.get('status')
                 if status:
                     user_error = "%s" % res_dict.get('status').get('message')
-                else: 
+                else:
                     user_error = "Error, no status message available"
             data = {
                 'customerNr': self.employee_id.address_id.legacy_no
@@ -53,7 +54,7 @@ class CIAMUpdate(models.TransientModel):
                 status = res_dict.get('status')
                 if status:
                     cust_error = "%s" % res_dict.get('status').get('message')
-                else: 
+                else:
                     cust_error = "Error, no status message available"
             if cust_id and user_id:
                 data = {
@@ -61,7 +62,7 @@ class CIAMUpdate(models.TransientModel):
                     'custId': cust_id,
                     'roleName': 'DAFA_COACH'
                 }
-                ciam_id.role_assign(data)    
+                ciam_id.role_assign(data)
             else:
-                error = "%s %s" %(user_error, cust_error)
+                error = "%s %s" % (user_error, cust_error)
                 raise Warning(error)
