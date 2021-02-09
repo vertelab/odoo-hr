52f94adaf3190e5ea1a1dcb4f4e8c222ee8995b9
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2021-02-09 11:53:03 +0100

    hr_user_ciam_update AFC-1723: added extra checks and balances

--- hr_user_ciam_update/__manifest__.py
+++ hr_user_ciam_update/__manifest__.py
@@ -1,15 +1,19 @@
 {
-    'name': 'HR User CIAM Update',
-    'version': '12.0.0.2',
-    'category': 'Human resources',
-    'description': """This module adds a button that runs an action that adds the user to the CIAM server""",
-    'author': 'Vertel AB',
-    'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
-    'depends': ['hr_employee_ciam_client', 'hr_employee_legacy_id', 'hr_employee_firstname_extension', 'partner_legacy_id'],
-    'data': [
-        'views/hr_employee_view.xml'
-
+    "name": "HR User CIAM Update",
+    "version": "12.0.0.2",
+    "category": "Human resources",
+    "description": """This module adds a button that runs an action that adds the user to the CIAM server""",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "http://www.vertel.se",
+    "depends": [
+        "hr_employee_ciam_client",
+        "hr_employee_legacy_id",
+        "hr_employee_firstname_extension",
+        "partner_legacy_id",
+        "hr_employee_ssn",
+        "auth_saml_ol",
     ],
-    'installable': True,
+    "data": ["views/hr_employee_view.xml"],
+    "installable": True,
 }
