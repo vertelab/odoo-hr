78389ff60d0cc1ccc45648de915defa4d0dfe1e9
Daniel Eriksson <daer@daer.io>
2021-05-21 13:14:08 +0200

    hr_office AFC-2334: made a few fields readonly.

--- hr_office/__manifest__.py
+++ hr_office/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name': 'HR Department offices',
-    'version': '12.0.0.3',
+    'version': '12.0.0.3.1',
     'category': 'Human resources',
       "description": """
     Offices
-    12.0.0.2 views \n
-    12.0.0.3 added group_no_one to menues to hide them from internal users \n
+    v12.0.0.2 views \n
+    v12.0.0.3 added group_no_one to menues to hide them from internal users \n
+    v12.0.0.3.1 AFC-2334: made a few fields readonly. \n
     """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
