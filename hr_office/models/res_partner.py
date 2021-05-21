c3ca63f78db314d8fb08273f2814ade79874e195
Sebastian Gieser <sebastian.gieser@arbetsformedlingen.se>
2021-05-21 15:33:38 +0200

    hr_office AFC-2339: fixed the way office_codes and operation_names is calculated, fixed setting of groups

--- hr_office/models/res_partner.py
+++ hr_office/models/res_partner.py
@@ -57,38 +57,29 @@ class ResUsers(models.Model):
 
     operation_names = fields.Char(
         string="Operations",
-        compute="compute_operation_names",
-        store=True,
-        readonly=True,
-    )
 
-    @api.depends("operation_ids")
-    @api.one
-    def compute_operation_names(self):
-        operation_names = []
-        for operation in self.operation_ids:
-            operation_names.append(operation.name)
-        if operation_names:
-            self.operation_names = ",".join([str(code) for code in operation_names])
-        else:
-            self.operation_names = ""
+    )
 
     office_codes = fields.Char(
-        string="Office codes", compute="compute_office_codes", store=True, readonly=True
+        string="Office codes",
+        compute="compute_office_codes"
     )
 
-    @api.depends("office_ids")
-    @api.one
+    @api.multi
     def compute_office_codes(self):
-        office_codes = []
-        for office in self.office_ids:
-            office_codes.append(office.office_code)
-        if office_codes:
-            self.office_codes = ",".join([str(code) for code in office_codes])
-        else:
-            self.office_codes = ""
+        for rec in self:
+            office_codes = []
+            for e in rec.employee_ids:
+                office_codes.append(e.office_codes)
+            rec.office_codes = ",".join(office_codes)
 
-
+    @api.multi
+    def compute_operation_names(self):
+        for rec in self:
+            operation_names = []
+            for e in rec.employee_ids:
+                operation_names.append(e.operation_names)
+            rec.operation_names = ",".join(operation_names)
 
 
 class HrEmployee(models.Model):
@@ -105,9 +96,15 @@ class HrEmployee(models.Model):
     )
 
     operation_names = fields.Char(
-        string="Operations", related="user_id.operation_names"
+        string="Operations",
+        compute="compute_operation_names",
+        readonly=True,
+        store=True
     )
-    office_codes = fields.Char(string="Office codes", related="user_id.office_codes")
+    office_codes = fields.Char(string="Office codes",
+                               compute="compute_office_codes",
+                               readonly=True,
+                               store=True)
     signature = fields.Char(string="Signature", related="user_id.login")
 
     @api.one
@@ -124,19 +121,53 @@ class HrEmployee(models.Model):
 
     @api.multi
     def write(self, vals):
-        res = super(HrEmployee, self).write(vals)
-        if "department_id" in vals:
-            self.update_office_ids()
-
-        if "0248" in self.office_codes and not self.user_id.has_group('af_security.af_meeting_planner_PDM'):
-            self.user_id.write({
-                'group_ids': [(4, 'af_security.af_meeting_planner_PDM', 0)]
-            })
-        elif "0248" not in self.office_codes and self.user_id.has_group('af_security.af_meeting_planner_PDM'):
-            self.user_id.write({
-                'group_ids': [(3, 'af_security.af_meeting_planner_PDM', 0)]
-            })
+        for rec in self:
+            res = super(HrEmployee, rec).write(vals)
+            if "department_id" in vals:
+                rec.update_office_ids()
+            rec.compute_office_codes()
+            rec.compute_operation_names()
+            rec.compute_is_pdm_planner()
         return vals
+
+    @api.multi
+    def compute_operation_names(self):
+        for rec in self:
+            operation_names = []
+            for operation in rec.operation_ids:
+                operation_names.append(operation.name)
+            if operation_names:
+                operation_name_string = ",".join([str(code) for code in operation_names])
+                if rec.operation_names != operation_name_string:
+                    rec.operation_names = operation_name_string
+            else:
+                rec.operation_names = ""
+
+    @api.multi
+    def compute_office_codes(self):
+        for rec in self:
+            office_codes = []
+            for office in rec.office_ids:
+                office_codes.append(office.office_code)
+            if office_codes:
+                office_codes_string = ",".join([str(code) for code in office_codes])
+                if office_codes_string != rec.office_codes:
+                    rec.office_codes = office_codes_string
+            else:
+                rec.office_codes = ""
+
+    @api.multi
+    def compute_is_pdm_planner(self):
+        for rec in self:
+            _logger.info("office_codes %s" % rec.office_codes)
+            if "0248" in rec.office_codes and not rec.user_id.has_group('af_security.af_meeting_planner_PDM'):
+                rec.user_id.write({
+                    'groups_id': [(4, self.env.ref('af_security.af_meeting_planner_PDM').id, 0)]
+                })
+            elif "0248" not in rec.office_codes and rec.user_id.has_group('af_security.af_meeting_planner_PDM'):
+                rec.user_id.write({
+                    'groups_id': [(3, self.env.ref('af_security.af_meeting_planner_PDM').id, 0)]
+                })
 
     @api.model_create_multi
     @api.returns("self", lambda value: value.id)
