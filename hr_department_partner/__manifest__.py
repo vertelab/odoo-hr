dfe572a95cb50c4b66db5ad7e2e87c36aa9958bf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-25 14:58:00 +0200

    Update __manifest__.py

--- hr_department_partner/__manifest__.py
+++ hr_department_partner/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,9 +14,8 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
-
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -24,14 +23,13 @@
     'name': 'HR Partner Department',
     'summary': 'To be able to have multiple addresses on department and select them on employee.',
     'author': 'Vertel AB',
-    'contributor': 'Mitchell Admin',
+    'contributor': '',
     'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-hr.git',
-    'category': 'Employee',
+    'repository': 'https://github.com/vertelab/odoo-hr',
     'version': '14.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'website': 'https://www.vertel.se',
     'license': 'AGPL-3',
+    'website': 'https://vertel.se/apps/hr',
     'description': """
         To be able to have multiple addresses on department and select them on employee.
     """,
