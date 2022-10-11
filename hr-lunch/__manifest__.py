a6fd72891440ad763fa94a77586cf0178c3517b4
Dmitri Iselund <Dmitri.iselund@gmail.com>
2022-10-11 12:02:38 +0000

    new hr lunch module to vote for restaurants

---
+++ hr-lunch/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Vote for lunch",
+    'version': "1.0",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/restaurant_choice.xml' 
+    ],
+}
---
+++ hr-lunch/models/lunch_choice.py
@@ -0,0 +1,5 @@
+from odoo import api, models, fields
+import logging
+ 
+_logger = logging.Logger('.........')
+
