179439734cab51137467d5413a93e1d387739cce
Emma <emma@emcy.se>
2022-10-13 08:40:39 +0000

    lunch choice menu update

--- hr-lunch/models/lunch_choice.py
+++ hr-lunch/models/lunch_choice.py
@@ -8,4 +8,8 @@ class LunchFields(models.Model):
 
     name = fields.Char(string="Name of restaurant")
     link_to_menu = fields.Char(string="Link to menu")
-    vote_bool = fields.Boolean(string="Vote status")
+    voted_on = fields.One2many("res.partner", "display_name", "Employees")
+    
+    def voted(self):
+        for rec in self:
+            _logger.warning("you have voted, did you really want to vote?")
