254cc1fc348a5ab6c350426d9c2763f7c0065c97
Emma <emma@emcy.se>
2022-10-25 09:47:02 +0000

    clear all-function, adress-field and user-timestamp in list

--- hr_lunch/models/lunch_choice.py
+++ hr_lunch/models/lunch_choice.py
@@ -1,23 +1,67 @@
 from odoo import api, models, fields, _
 import logging
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 import validators
 from validators import ValidationFailure
+from datetime import datetime
+import pytz
 
 _logger = logging.getLogger(__name__)
 
 class ResUsers(models.Model):
     _inherit = "res.users"
+    
+    context = fields.Char(string="Context", compute="_get_context")
+    user_datetime = fields.Datetime(compute="user_date")
     lunch_id = fields.Many2many("lunch.choice")
+
+    @api.onchange('lunch_id')
+    def user_date(self):
+        updates = self.env["res.users"].search([])
+        user = self.env["res.users"].browse(self.env.uid)
+        #_logger.warning(f"{user.id}")
+        user_tz = self.env.user.tz or pytz.utc
+        local = pytz.timezone(user_tz)
+        current_time = datetime.now()
+        trimmed = current_time.isoformat(' ', 'seconds')
+        _logger.warning(f"{local}")
+        _logger.warning(f"trimmed version: {trimmed}")
+        display_date_result = datetime.strftime(pytz.utc.localize(datetime.strptime(str(trimmed),
+            DEFAULT_SERVER_DATETIME_FORMAT)).astimezone(local),"%Y-%m-%d %H:%M:%S")
+        #_logger.warning(f"datetime-format: {display_date_result}")
+        for record in updates:
+            _logger.info(record.id)
+            if record.id == user.id:
+                record.update({"user_datetime": display_date_result})
+            else:
+                _logger.warning(f"user hasn't voted")
+                continue
+
+    def _get_context(self):
+        self.context = dict(self.env.context)
+        _logger.warning(f"{self.context}")
 
 class LunchFields(models.Model):
     _name = "lunch.choice"
+    
+    def clear_all(self):
+        if self.env.is_admin() == True:
+            _logger.warning(f"clear all was pressed")
+            restaurants = self.env["lunch.choice"].search([])
+            for rec in restaurants:
+                restaurante = self.env["lunch.choice"].browse(rec.id)
+                restaurante.update({"voted_on": [(5, )]})
+            return True
+        else:
+            _logger.warning(f"user is not admin")
+            return False
 
     def valid_url(self) -> bool:
         url_string = str(self.link_to_menu).strip()
-        _logger.error(self.link_to_menu)
+        #_logger.error(self.link_to_menu)
         result = validators.url(url_string)
         if isinstance(result, ValidationFailure):
-            _logger.warning(f"invalid url")
+            #_logger.warning(f"invalid url")
             return False
         return result
 
@@ -49,14 +93,14 @@ class LunchFields(models.Model):
         for record in self:
             _result = record.valid_url()
             if _result == True:
-                _logger.warning(f"{record.link_to_menu}")
+                #_logger.warning(f"{record.link_to_menu}")
                 return {
                     "type": "ir.actions.act_url", 
                     "url": record.link_to_menu,
                     "target": "new"
                 }
             else:
-                _logger.warning(f"{record.link_to_menu} is broken")
+                #_logger.warning(f"link to menu is invalid")
                 return f"url is not valid"
       
     @api.depends('voted_on')
@@ -70,9 +114,12 @@ class LunchFields(models.Model):
     user_id = fields.Integer(compute="get_current_user")
     voted_on  = fields.Many2many("res.users", string="Employees")
     show_button = fields.Boolean(compute="_show_button")
-    voter_id = fields.Integer(compute="list_all_voters")
+    voter_id = fields.Integer(compute="list_all_voters", store=True)
     menu_button = fields.Boolean(compute="_menu_button")
-    
+    address = fields.Char(string="address")
+    clear_button = fields.Boolean(compute="clear_all")
+   
+    @api.onchange('voted_on')
     def voted(self):
         for rec in self:
             self.update({"voted_on": [(4, self.env.user.id, 0)]})
@@ -83,6 +130,7 @@ class LunchFields(models.Model):
             restaurants = self.env["lunch.choice"].search([])
             for restaurant in restaurants:
                 if restaurant.id == choice:
+                    #add timestamp for when specific user voted on the option
                     continue
                 restaurante = self.env["lunch.choice"].browse(restaurant.id)
                 restaurante.update({"voted_on": [(3, self.env.user.id, 0)]})
