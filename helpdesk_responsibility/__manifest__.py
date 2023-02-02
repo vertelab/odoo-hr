8b84097658cc965163f37f3aefe1538dd4dd9599
Jakob Krabbe <jakob.krabbe@vertel.se>
2023-02-02 14:47:43 +0100

    helpdesk_responsibility/__manifest__.py: fixing with details

--- helpdesk_responsibility/__manifest__.py
+++ helpdesk_responsibility/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Helpdesk Responsibility',
+    'name': 'HR: Helpdesk Responsibility',
     'summary': 'To share responsibilities between helpdesk teams.',
     'author': 'Vertel AB',
     'contributor': '',
@@ -8,7 +8,7 @@
     'repository': 'https://github.com/vertelab/odoo-hr',
     'version': '14.0.0.2.0',
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/hr',
+    'website': 'https://vertel.se/apps/odoo-hr/helpdesk_responsibility',
     'description': """
         To share responsibilities between helpdesk teams.
     """,
