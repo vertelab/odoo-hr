7fa9859bad0a2a49123621ce09220b6b46982bef
MikaelAtVertel <mikael.carlsson@vertel.se>
2023-06-07 08:57:28 +0000

    Add the renamed module

---
+++ hr_website_about_us/__manifest__.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'HR: Website About Us',
+    'version': '14.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Show employee on webpage.',
+    'category': 'HR',
+    'description': """
+    To show or hide employee on public webpage.
+Edit settings for non-public users.
+Users and groups >> Groups.
+Click "Accesses".
+Add new line and enter Model (Employed) and "Read" access.
+
+See enclosed photos for description.
+
+* * *
+    When editing code, uninstall module for changes to take effect.
+     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-hr/hr_website_about-us',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-hr',
+    'depends': ['website_imagemagick','hr','website','web_domain_field',],
+    'data': [
+        'views/assets.xml',
+        'views/about_us_view.xml',
+        # 'views/hr_view.xml',
+        'data/data.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
