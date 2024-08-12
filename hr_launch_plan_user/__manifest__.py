eb03fe9a70553c9e26c2b0ea5249dc807b65c070
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-12 11:13:22 +0200

    Update __manifest__.py

--- hr_launch_plan_user/__manifest__.py
+++ hr_launch_plan_user/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,7 +21,7 @@
 
 {
     'name': 'HR: Launch Plan User',
-    'version': '14.0.0.0.0',
+    'version': '0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Launch Plan from hr.group_hr_user instead of hr.group_hr_manage',
     'category': 'HR',
