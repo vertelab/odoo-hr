2ee54cb2834333e22ec719a67f93847e097f945a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-12 11:19:01 +0200

    Update __manifest__.py

--- hr_personal_equipment_request/__manifest__.py
+++ hr_personal_equipment_request/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Hr Personal Equipment Request",
+    "name": "HR: Personal Equipment Request",
     "summary": """
         This addon allows to manage employee personal equipment""",
-    "version": "14.0.1.0.1",
+    "version": "0.1",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
