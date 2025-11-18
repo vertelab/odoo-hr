26bbc859d7d9f59c83d5836c791bab377fc1c030
marcuszxc <marcus.karlsson@vertel.se>
2025-11-18 09:56:07 +0000

    hr_tier_validation: Added base_tier_validation as a dep

--- hr_tier_validation/__manifest__.py
+++ hr_tier_validation/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/vertel/odoo-hr",
-    "depends": ['hr', 'project_purchase'],
+    "depends": ['hr', 'project_purchase','base_tier_validation'],
     "data": [
         'views/hr_department_views.xml',
         'views/tier_definition_views.xml',
