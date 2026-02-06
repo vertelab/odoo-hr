58d24a9208d3c5e7fb06f11014708ce4ac60b2ae
marcuszxc <marcus.karlsson@vertel.se>
2026-02-06 12:15:46 +0000

    Created hr_remove_attendance_barcode

---
+++ hr_remove_attendance_barcode/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) {year} {company} info@vertel.se
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#
+# https://www.odoo.com/documentation/14.0/reference/module.html
+#
+{
+    'name': 'HR: Remove Attendance Barcode',
+    'version': '1.0',
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.odoo.com""",
+    'category': '', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-',
+    'images': ['static/description/banner.png'], # 560x280
+    'license': 'AGPL-3',
+    'depends': ["hr_attendance"],
+    'data': [],
+    'demo': [],
+    'assets': {
+        'web.assets_backend': [
+            'hr_remove_attendance_barcode/static/src/**/*.js',
+            'hr_remove_attendance_barcode/static/src/**/*.xml',
+        ],
+    },
+    'application': False,
+    'installable': True,    
+    'auto_install': False,
+}
